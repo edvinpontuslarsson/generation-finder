971c4e1fe669448f96e9a0446429ce40e3ad9361
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2023-07-26 12:06:35 +0200

    app notes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
+import { useState } from 'react';
+
+// TODO everything in 1 file first, divide later
+
+// https://github.com/edvinpontuslarsson/hbg-works-kodtest/blob/ASP.NET_version/web/src/components/ApplicationForm.js
+// https://github.com/edvinpontuslarsson/hbg-works-kodtest/blob/ASP.NET_version/web/src/components/CompanySection.js
 
 function App() {
+  // const birthYear = 
+
   return (
     <div className="App">
       <h1>Generation Finder</h1>
