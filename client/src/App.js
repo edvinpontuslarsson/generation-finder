796aea15f8e086a657974d651fb9b1026149f7f5
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2023-07-26 18:39:57 +0200

    TODOS and notes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { useState } from 'react';
-
 import classnames from 'classnames';
+import { getGenerationsData } from './api/api';
 
 // TODO everything in 1 file first, divide later
 
@@ -12,6 +12,18 @@ function App() {
   const [birthYear, setBirthYear] = useState(0);
   // TODO implement error handling
   const [invalidBirthYear, setInvalidBirthYear] = useState(false);
+
+  const generationsData = getGenerationsData();
+
+  // TODO see here form validation & err msgs
+  // context too much here
+  // https://www.freecodecamp.org/news/how-to-validate-forms-in-react/
+
+  // TODO move this into jsx for below input and use state
+  const userGeneration = generationsData.generations.find(
+    //       1991        1981           1991        1996
+    (gen) => birthYear > gen.minYear && birthYear < gen.maxYear
+  );
 
   return (
     <div className="App">
@@ -26,7 +38,7 @@ function App() {
           onChange={(event) => {
             setBirthYear(event.target.value);
           }}
-          // TODO if this doesn't work, see usage on line 89 here: 
+          // TODO if this doesn't work, see usage on line 89 here:
           // https://github.com/edvinpontuslarsson/hbg-works-kodtest/blob/ASP.NET_version/web/src/components/CompanySection.js
           className={classnames({
             'error-border': invalidBirthYear,
