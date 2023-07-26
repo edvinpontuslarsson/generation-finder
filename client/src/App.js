b31f06f53a0656b9d6e5cd80b5ba31b9626e770a
Edvin Larsson <edvinpontuslarsson@gmail.com>
2023-07-26 21:38:36 +0200

    Inital web app (#2)
    * removes inital react stuff, adds initial input html
    
    * changes html title
    
    * app notes
    
    * api in  progress with notes
    
    * todo and some generation data
    
    * generations[] done
    
    * removes ref repo comment
    
    * currentYear
    
    * sorts
    
    * input type nr
    
    * installs classnames
    
    * todos etc
    
    * .error-border border-color: red
    
    * TODOS and notes
    
    * happypath implemented
    
    * clarificaties boomers
    
    * can go again button after result
    
    * uninstalls classnames
    
    * removes classnames stuff
    
    * error-label
    
    * error-label css
    
    * corrects generation finding bug
    
    * uses classnames for error-border
    
    * removes todo comments
    
    * some css => .birth-year-input margin-bottom: 20px

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import classnames from 'classnames';
+import { getGenerationsData } from './api/api';
 
 function App() {
+  const [birthYear, setBirthYear] = useState(0);
+  const [invalidInput, setInvalidInput] = useState(false);
+  const [userGeneration, setUserGeneration] = useState(null);
+  const [displayInput, setDisplayInput] = useState(true);
+
+  const generationsData = getGenerationsData();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Generation Finder</h1>
+      {displayInput && (
+        <>
+          <p>
+            Please input your year of birth to see which generation you belong
+            to.
+          </p>
+          <div>
+            <input
+              type="number"
+              value={birthYear}
+              onChange={(event) => {
+                setBirthYear(event.target.value);
+              }}
+              className={`birth-year-input ${classnames({
+                'error-border': invalidInput,
+              })}`}
+            />
+            {invalidInput && (
+              <label className="error-label">
+                Please enter a 4 digit number between{' '}
+                {generationsData.lowestMinYear} and{' '}
+                {generationsData.highestMaxYear}
+              </label>
+            )}
+          </div>
+
+          <button
+            onClick={() => {
+              const foundGeneration = generationsData.generations.find(
+                // e.g.
+                //       1991         1981           1991        1996
+                (gen) => birthYear >= gen.minYear && birthYear <= gen.maxYear
+              );
+
+              if (foundGeneration) {
+                setUserGeneration(foundGeneration);
+
+                setInvalidInput(false);
+                setDisplayInput(false);
+              } else {
+                setInvalidInput(true);
+              }
+            }}
+          >
+            Submit
+          </button>
+        </>
+      )}
+
+      {!displayInput && (
+        <>
+          <h2>You belong to {userGeneration.title}</h2>
+          <p>
+            Born between {userGeneration.minYear} and {userGeneration.maxYear}
+          </p>
+          {userGeneration.famousExamples && (
+            <>
+              <h3>Famous examples:</h3>
+              <ul>
+                {userGeneration.famousExamples.map((ex) => (
+                  <li>{ex}</li>
+                ))}
+              </ul>
+            </>
+          )}
+          <button
+            onClick={() => {
+              setBirthYear(0);
+              setUserGeneration(null);
+
+              setDisplayInput(true);
+            }}
+          >
+            Try again with a different birthyear!
+          </button>
+        </>
+      )}
     </div>
   );
 }
---
+++ client/src/api/api.js
@@ -0,0 +1,60 @@
+function getGenerationsData() {
+  const currentYear = new Date().getFullYear();
+
+  const generations = [
+    {
+      title: 'Generation Alpha',
+      minYear: 2013,
+      maxYear: currentYear,
+      famousExamples: false,
+    },
+    {
+      title: 'Generation Z',
+      minYear: 1997,
+      maxYear: 2012,
+      famousExamples: ['MrBeast', 'Greta Thunberg'],
+    },
+    {
+      title: 'Millenials (Generation Y)',
+      minYear: 1981,
+      maxYear: 1996,
+      famousExamples: ['Taylor Swift', 'Mark Zuckerberg'],
+    },
+    {
+      title: 'Generation X',
+      minYear: 1965,
+      maxYear: 1980,
+      famousExamples: ['Jennifer Lopez', 'Elon Musk'],
+    },
+    {
+      title: 'Baby Boomers (sometimes shortened to boomers)',
+      minYear: 1946,
+      maxYear: 1964,
+      famousExamples: ['Arnold Schwarzenegger,', 'Oprah Winfrey'],
+    },
+    {
+      title: 'The Silent Generation',
+      minYear: 1928,
+      maxYear: 1945,
+      famousExamples: ['Jane Fonda', 'Morgan Freeman'],
+    },
+    {
+      title: 'The Greatest Generation',
+      minYear: 1901,
+      maxYear: 1927,
+      famousExamples: ['Greta Garbo', 'Alan Watts'],
+    },
+  ].sort((a, b) => a.minYear < b.minYear);
+
+  const lowestMinYear = generations[0].minYear;
+  const endIndex = generations.length - 1;
+  const highestMaxYear = generations[endIndex].maxYear;
+
+  return {
+    lowestMinYear,
+    highestMaxYear,
+    generations,
+  };
+}
+
+export { getGenerationsData };
