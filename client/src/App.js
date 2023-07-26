602c8229ee0769fe01429f9db889e5de8336e5a3
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2023-07-26 17:52:13 +0200

    todos etc

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { useState } from 'react';
 
+import classnames from 'classnames';
+
 // TODO everything in 1 file first, divide later
 
 // https://github.com/edvinpontuslarsson/hbg-works-kodtest/blob/ASP.NET_version/web/src/components/ApplicationForm.js
@@ -8,6 +10,8 @@ import { useState } from 'react';
 
 function App() {
   const [birthYear, setBirthYear] = useState(0);
+  // TODO implement error handling
+  const [invalidBirthYear, setInvalidBirthYear] = useState(false);
 
   return (
     <div className="App">
@@ -16,8 +20,17 @@ function App() {
         Please input your year of birth to see which generation you belong to.
       </p>
       <div>
-        <input 
-          type='number'
+        <input
+          type="number"
+          value={birthYear}
+          onChange={(event) => {
+            setBirthYear(event.target.value);
+          }}
+          // TODO if this doesn't work, see usage on line 89 here: 
+          // https://github.com/edvinpontuslarsson/hbg-works-kodtest/blob/ASP.NET_version/web/src/components/CompanySection.js
+          className={classnames({
+            'error-border': invalidBirthYear,
+          })}
         />
       </div>
     </div>
