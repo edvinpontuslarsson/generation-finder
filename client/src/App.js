4833eaabd9ad1d789ba9c100bf20893a3c75caaa
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2023-08-01 13:41:19 +0200

    initializes firebase

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,24 @@ import { useState } from 'react';
 import classnames from 'classnames';
 import { getGenerationsData } from './api/api';
 
-function App() {
-  console.log(process.env.REACT_APP_HELLO);
+/**
+ * INITIALIZES FIREBASE 
+ */
 
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+initializeApp(firebaseConfig);
+
+function App() {
   const [birthYear, setBirthYear] = useState(0);
   const [invalidInput, setInvalidInput] = useState(false);
   const [userGeneration, setUserGeneration] = useState(null);
@@ -46,7 +61,7 @@ function App() {
             onClick={() => {
               const foundGeneration = generationsData.generations.find(
                 // e.g.
-                //       1991         1981           1991        1996
+                //       1991         1981           1991         1996
                 (gen) => birthYear >= gen.minYear && birthYear <= gen.maxYear
               );
 
