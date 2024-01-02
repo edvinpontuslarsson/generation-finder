77ec9c38928fded5d547b2801ed208cc6453a401
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2024-01-02 15:36:23 +0100

    api objects

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -10,39 +10,110 @@ function getGenerationsData() {
       title: 'Generation Z',
       minYear: 1997,
       maxYear: 2012,
-      famousExamples: ['MrBeast', 'Greta Thunberg'],
+      famousExamples: [
+        {
+          name: 'MrBeast',
+          birthYear: 'XXXX',
+          wikiLink: 'TODO Insert real wiki link here',
+        },
+        {
+          name: 'Greta Thunberg',
+          birthYear: 'XXXX',
+          wikiLink: 'TODO Insert real wiki link here',
+        }
+      ]
     },
     {
       title: 'Millenials (Generation Y)',
       minYear: 1981,
       maxYear: 1996,
-      famousExamples: ['Taylor Swift', 'Mark Zuckerberg'],
+      famousExamples: [
+        {
+          name: 'Taylor Swift',
+          birthYear: 'XXXX',
+          wikiLink: 'TODO Insert real wiki link here',
+        },
+        {
+          name: 'Mark Zuckerberg',
+          birthYear: 'XXXX',
+          wikiLink: 'TODO Insert real wiki link here',
+        }
+      ]
     },
     {
       title: 'Generation X',
       minYear: 1965,
       maxYear: 1980,
-      famousExamples: ['Jennifer Lopez', 'Elon Musk'],
+      famousExamples: [
+        {
+          name: 'Jennifer Lopez',
+          birthYear: 'XXXX',
+          wikiLink: 'TODO Insert real wiki link here',
+        },
+        {
+          name: 'Elon Musk',
+          birthYear: 'XXXX',
+          wikiLink: 'TODO Insert real wiki link here',
+        }
+      ]
     },
     {
       title: 'Baby Boomers (sometimes shortened to boomers)',
       minYear: 1946,
       maxYear: 1964,
-      famousExamples: ['Arnold Schwarzenegger,', 'Oprah Winfrey'],
+      famousExamples: [',', ''],
+      famousExamples: [
+        {
+          name: 'Arnold Schwarzenegger',
+          birthYear: 'XXXX',
+          wikiLink: 'TODO Insert real wiki link here',
+        },
+        {
+          name: 'Oprah Winfrey',
+          birthYear: 'XXXX',
+          wikiLink: 'TODO Insert real wiki link here',
+        }
+      ]
     },
     {
       title: 'The Silent Generation',
       minYear: 1928,
       maxYear: 1945,
-      famousExamples: ['Jane Fonda', 'Morgan Freeman'],
+      famousExamples: [
+        {
+          name: 'Jane Fonda',
+          birthYear: 'XXXX',
+          wikiLink: 'TODO Insert real wiki link here',
+        },
+        {
+          name: 'Morgan Freeman',
+          birthYear: 'XXXX',
+          wikiLink: 'TODO Insert real wiki link here',
+        }
+      ]
     },
     {
       title: 'The Greatest Generation',
       minYear: 1901,
       maxYear: 1927,
-      famousExamples: ['Greta Garbo', 'Alan Watts'],
+      famousExamples: [
+        {
+          name: 'Greta Garbo',
+          birthYear: 'XXXX',
+          wikiLink: 'TODO Insert real wiki link here',
+        },
+        {
+          name: 'Alan Watts',
+          birthYear: 'XXXX',
+          wikiLink: 'TODO Insert real wiki link here',
+        }
+      ]
     },
   ].sort((a, b) => a.minYear - b.minYear);
+
+  // TODO fix in jsx component also, to handle objects
+  // previously famousExamples: boolean | string[]
+  // now famousExamples: boolean | celebrityObject[]
 
   const lowestMinYear = generations[0].minYear;
   const endIndex = generations.length - 1;
