00552988e8da313febdac7cb7ea3037bc7cf54ed
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2024-01-02 15:46:32 +0100

    real years & links

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -13,13 +13,13 @@ function getGenerationsData() {
       famousExamples: [
         {
           name: 'MrBeast',
-          birthYear: 'XXXX',
-          wikiLink: 'TODO Insert real wiki link here',
+          birthYear: '1998',
+          wikiLink: 'https://en.wikipedia.org/wiki/MrBeast',
         },
         {
           name: 'Greta Thunberg',
-          birthYear: 'XXXX',
-          wikiLink: 'TODO Insert real wiki link here',
+          birthYear: '2003',
+          wikiLink: 'https://en.wikipedia.org/wiki/Greta_Thunberg',
         }
       ]
     },
@@ -30,13 +30,13 @@ function getGenerationsData() {
       famousExamples: [
         {
           name: 'Taylor Swift',
-          birthYear: 'XXXX',
-          wikiLink: 'TODO Insert real wiki link here',
+          birthYear: '1989',
+          wikiLink: 'https://en.wikipedia.org/wiki/Taylor_Swift',
         },
         {
           name: 'Mark Zuckerberg',
-          birthYear: 'XXXX',
-          wikiLink: 'TODO Insert real wiki link here',
+          birthYear: '1984',
+          wikiLink: 'https://en.wikipedia.org/wiki/Mark_Zuckerberg',
         }
       ]
     },
@@ -47,13 +47,13 @@ function getGenerationsData() {
       famousExamples: [
         {
           name: 'Jennifer Lopez',
-          birthYear: 'XXXX',
-          wikiLink: 'TODO Insert real wiki link here',
+          birthYear: '1969',
+          wikiLink: 'https://en.wikipedia.org/wiki/Jennifer_Lopez',
         },
         {
           name: 'Elon Musk',
-          birthYear: 'XXXX',
-          wikiLink: 'TODO Insert real wiki link here',
+          birthYear: '1971',
+          wikiLink: 'https://en.wikipedia.org/wiki/Elon_Musk',
         }
       ]
     },
@@ -61,17 +61,16 @@ function getGenerationsData() {
       title: 'Baby Boomers (sometimes shortened to boomers)',
       minYear: 1946,
       maxYear: 1964,
-      famousExamples: [',', ''],
       famousExamples: [
         {
           name: 'Arnold Schwarzenegger',
-          birthYear: 'XXXX',
-          wikiLink: 'TODO Insert real wiki link here',
+          birthYear: '1947',
+          wikiLink: 'https://en.wikipedia.org/wiki/Arnold_Schwarzenegger',
         },
         {
           name: 'Oprah Winfrey',
-          birthYear: 'XXXX',
-          wikiLink: 'TODO Insert real wiki link here',
+          birthYear: '1954',
+          wikiLink: 'https://en.wikipedia.org/wiki/Oprah_Winfrey',
         }
       ]
     },
@@ -82,13 +81,13 @@ function getGenerationsData() {
       famousExamples: [
         {
           name: 'Jane Fonda',
-          birthYear: 'XXXX',
-          wikiLink: 'TODO Insert real wiki link here',
+          birthYear: '1937',
+          wikiLink: 'https://en.wikipedia.org/wiki/Jane_Fonda',
         },
         {
           name: 'Morgan Freeman',
-          birthYear: 'XXXX',
-          wikiLink: 'TODO Insert real wiki link here',
+          birthYear: '1937',
+          wikiLink: 'https://en.wikipedia.org/wiki/Morgan_Freeman',
         }
       ]
     },
@@ -99,21 +98,17 @@ function getGenerationsData() {
       famousExamples: [
         {
           name: 'Greta Garbo',
-          birthYear: 'XXXX',
-          wikiLink: 'TODO Insert real wiki link here',
+          birthYear: '1905',
+          wikiLink: 'https://en.wikipedia.org/wiki/Greta_Garbo',
         },
         {
           name: 'Alan Watts',
-          birthYear: 'XXXX',
-          wikiLink: 'TODO Insert real wiki link here',
+          birthYear: '1915',
+          wikiLink: 'https://en.wikipedia.org/wiki/Alan_Watts',
         }
       ]
     },
   ].sort((a, b) => a.minYear - b.minYear);
-
-  // TODO fix in jsx component also, to handle objects
-  // previously famousExamples: boolean | string[]
-  // now famousExamples: boolean | celebrityObject[]
 
   const lowestMinYear = generations[0].minYear;
   const endIndex = generations.length - 1;
