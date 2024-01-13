16379d12cd773d72342beca1f92bd6f80cf7e7d9
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2024-01-13 19:21:16 +0100

    removes redundant lower border value analyses

--- client/cypress/e2e/generationFinder.cy.ts
+++ client/cypress/e2e/generationFinder.cy.ts
@@ -41,23 +41,11 @@ describe('Tests of the generation finder', () => {
     cy.celebrityListContainsExpectedContent();
   });
 
+  // Lower border value analysis is only necessary for the earliest generation
+  // because the lower border value analysis of each generation is identical to
+  // the upper border value analysis of its previous generation
+
   // The Silent Generation: 1928 - 1945
-
-  it('Lower border value analysis for Silent Generation, 1927 & 1928', () => {
-    cy.get('#birth-year-input').clear().type('1927');
-    cy.get('#generation-finder-find-out-button').click();
-
-    cy.get('#found-generation-heading').contains(/greatest generation/i);
-    cy.celebrityListContainsExpectedContent();
-
-    cy.get('#found-generation-try-again-button').click();
-
-    cy.get('#birth-year-input').clear().type('1928');
-    cy.get('#generation-finder-find-out-button').click();
-
-    cy.get('#found-generation-heading').contains(/silent generation/i);
-    cy.celebrityListContainsExpectedContent();
-  });
 
   it('Upper border value analysis for Silent Generation, 1945 & 1946', () => {
     cy.get('#birth-year-input').clear().type('1945');
@@ -73,26 +61,9 @@ describe('Tests of the generation finder', () => {
 
     cy.get('#found-generation-heading').contains(/boomer/i);
     cy.celebrityListContainsExpectedContent();
-  })
-
+  });
 
   // Baby Boomers: 1946 - 1964
-
-  it('Lower border value analysis for Baby Boomers, 1945 & 1946', () => {
-    cy.get('#birth-year-input').clear().type('1945');
-    cy.get('#generation-finder-find-out-button').click();
-
-    cy.get('#found-generation-heading').contains(/silent generation/i);
-    cy.celebrityListContainsExpectedContent();
-
-    cy.get('#found-generation-try-again-button').click();
-
-    cy.get('#birth-year-input').clear().type('1946');
-    cy.get('#generation-finder-find-out-button').click();
-
-    cy.get('#found-generation-heading').contains(/boomer/i);
-    cy.celebrityListContainsExpectedContent();
-  });
 
   it('Upper border value analysis for Baby Boomers, 1964 & 1965', () => {
     cy.get('#birth-year-input').clear().type('1964');
@@ -108,25 +79,9 @@ describe('Tests of the generation finder', () => {
 
     cy.get('#found-generation-heading').contains(/generation x/i);
     cy.celebrityListContainsExpectedContent();
-  })
+  });
 
   // Generation X: 1965 - 1980
-
-  it('Lower border value analysis for Generation X, 1964 & 1965', () => {
-    cy.get('#birth-year-input').clear().type('1964');
-    cy.get('#generation-finder-find-out-button').click();
-
-    cy.get('#found-generation-heading').contains(/boomer/i);
-    cy.celebrityListContainsExpectedContent();
-
-    cy.get('#found-generation-try-again-button').click();
-
-    cy.get('#birth-year-input').clear().type('1965');
-    cy.get('#generation-finder-find-out-button').click();
-
-    cy.get('#found-generation-heading').contains(/generation x/i);
-    cy.celebrityListContainsExpectedContent();
-  });
 
   it('Upper border value analysis for Generation X, 1980 & 1981', () => {
     cy.get('#birth-year-input').clear().type('1980');
@@ -142,25 +97,9 @@ describe('Tests of the generation finder', () => {
 
     cy.get('#found-generation-heading').contains(/millenial/i);
     cy.celebrityListContainsExpectedContent();
-  })
+  });
 
   // Millenials: 1981 - 1996
-
-  it('Lower border value analysis for Millenials, 1980 & 1981', () => {
-    cy.get('#birth-year-input').clear().type('1980');
-    cy.get('#generation-finder-find-out-button').click();
-
-    cy.get('#found-generation-heading').contains(/generation x/i);
-    cy.celebrityListContainsExpectedContent();
-
-    cy.get('#found-generation-try-again-button').click();
-
-    cy.get('#birth-year-input').clear().type('1981');
-    cy.get('#generation-finder-find-out-button').click();
-
-    cy.get('#found-generation-heading').contains(/millenial/i);
-    cy.celebrityListContainsExpectedContent();
-  });
 
   it('Upper border value analysis for Millenials, 1996 & 1997', () => {
     cy.get('#birth-year-input').clear().type('1996');
@@ -176,25 +115,9 @@ describe('Tests of the generation finder', () => {
 
     cy.get('#found-generation-heading').contains(/z/i);
     cy.celebrityListContainsExpectedContent();
-  })
+  });
 
   // Generation Z: 1997 - 2012
-
-  it('Lower border value analysis for Generation Z, 1996 & 1997', () => {
-    cy.get('#birth-year-input').clear().type('1996');
-    cy.get('#generation-finder-find-out-button').click();
-
-    cy.get('#found-generation-heading').contains(/millenial/i);
-    cy.celebrityListContainsExpectedContent();
-
-    cy.get('#found-generation-try-again-button').click();
-
-    cy.get('#birth-year-input').clear().type('1997');
-    cy.get('#generation-finder-find-out-button').click();
-
-    cy.get('#found-generation-heading').contains(/z/i);
-    cy.celebrityListContainsExpectedContent();
-  });
 
   it('Upper border value analysis for Generation Z, 2012 & 2013', () => {
     cy.get('#birth-year-input').clear().type('2012');
@@ -210,25 +133,9 @@ describe('Tests of the generation finder', () => {
 
     cy.get('#found-generation-heading').contains(/generation alpha/i);
     cy.celebrityListContainsExpectedContent();
-  })
+  });
 
   // Generation Alpha: 2013 - 2024
-
-  it('Lower border value analysis for generation Alpha, 2012 & 2013', () => {
-    cy.get('#birth-year-input').clear().type('2012');
-    cy.get('#generation-finder-find-out-button').click();
-
-    cy.get('#found-generation-heading').contains(/z/i);
-    cy.celebrityListContainsExpectedContent();
-
-    cy.get('#found-generation-try-again-button').click();
-
-    cy.get('#birth-year-input').clear().type('2013');
-    cy.get('#generation-finder-find-out-button').click();
-
-    cy.get('#found-generation-heading').contains(/generation alpha/i);
-    cy.celebrityListContainsExpectedContent();
-  })
 
   it('Upper border value analysis for generation Alpha, 2024 & 2025', () => {
     cy.get('#birth-year-input').clear().type('2025');
