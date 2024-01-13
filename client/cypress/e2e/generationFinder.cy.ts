ab42b9d79d58240d8ff73b4be9c8444b77f8e6fa
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2024-01-13 18:18:27 +0100

    Upper border value analysis for generation Alpha, 2024 & 2025

--- client/cypress/e2e/generationFinder.cy.ts
+++ client/cypress/e2e/generationFinder.cy.ts
@@ -40,6 +40,16 @@ describe('Tests of the generation finder', () => {
   });
 
   it('Upper border value analysis for generation Alpha, 2024 & 2025', () => {
-    // TODO
+    cy.get('#birth-year-input').clear().type('2025');
+    cy.get('#generation-finder-find-out-button').click();
+    cy.get('#birth-year-error-label').should('exist');
+
+    cy.get('#birth-year-input').clear().type('2024');
+    cy.get('#generation-finder-find-out-button').click();
+    cy.get('#birth-year-error-label').should('not.exist');
+
+    cy.get('#found-generation-heading').contains(/generation alpha/i);
+
+    cy.celebrityListContainsExpectedContent();
   });
 });
