6a89d9b9408cb5877683a0bd7cc2c4968a003bc0
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2024-01-13 23:18:31 +0100

    js docs comments

--- client/cypress/support/commands.ts
+++ client/cypress/support/commands.ts
@@ -1,3 +1,8 @@
+/**
+ * Asserts that:
+ * The error label is not initially present.
+ * The error label should exist after this invalid input.
+ */
 function invalidBirthYearInputTest(invalidInput?: number | string) {
   cy.get('#birth-year-error-label').should('not.exist');
 
@@ -10,6 +15,13 @@ function invalidBirthYearInputTest(invalidInput?: number | string) {
   cy.get('#birth-year-error-label').should('exist');
 }
 
+/**
+ * Asserts that:
+ * Birth year form exists and work as expected.
+ * The error label is not present.
+ * The correct generation is displayed after birth year submission.
+ * Celebrity list exists and contains expected child elements.
+ */
 function happyPathBirthYearTest(birthYear: number, expectedGeneration: RegExp) {
   cy.get('#birth-year-input').clear().type(`${birthYear}`);
   cy.get('#generation-finder-find-out-button').click();
