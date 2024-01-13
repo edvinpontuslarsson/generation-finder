1c23a354cbad2d880f262856e9548568ad9c8978
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2024-01-13 16:56:07 +0100

    correctly fails for year 1900

--- client/cypress/e2e/generationFinder.cy.ts
+++ client/cypress/e2e/generationFinder.cy.ts
@@ -12,4 +12,18 @@ describe('Tests of the generation finder', () => {
     cy.get('#generation-finder-find-out-button').click();
     cy.get('#birth-year-error-label').should('exist');
   });
+
+  // border value analysis greatest generation, err msg first
+  it('Lower border analysis for greatest generation, 1900 & 1901', () => {
+    cy.get('#birth-year-error-label').should('not.exist');
+
+    cy.get('#birth-year-input').clear().type('1900');
+    cy.get('#generation-finder-find-out-button').click();
+    cy.get('#birth-year-error-label').should('exist');
+  });
+
+  // TODO 1
+  // for upper border value analysis, start with too high
+
+  // TODO 2 maybe I can have reusable assertion for ensuring display is correct
 });
