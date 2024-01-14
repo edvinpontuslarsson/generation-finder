152a195418efd2ce386a58eea4308c55476b6be9
Edvin Larsson <edvinpontuslarsson@gmail.com>
2024-01-14 13:49:03 +0100

    Border value analysis (#19)
    * Error message if empty birth year
    
    * correctly fails for year 1900
    
    * border value analysis for greatest generation in progress
    
    * todos etc
    
    * gound generation heading
    
    * The celebrity list should contain at least 1 element
    
    * cy.wrap(element).find('img').should('exist')
    
    * celeb list asserts each child element should contain expected content
    
    * err msg should not exist when in view
    
    * reusable function celebrityListContainsExpectedContent
    
    * Upper border analysis for greatest generation done
    
    * no longer testing title, too simple
    
    * Upper border value analysis for generation Alpha, 2024 & 2025
    
    * comments headings
    
    * Lower border value analysis for Silent Generation, 1927 & 1928
    
    * Upper border value analysis for Silent Generation, 1945 & 1946
    
    * Lower border value analysis for generation Alpha, 2012 & 2013
    
    * border value analysis for Baby Boomers
    
    * border value analysis for Millenials
    
    * all border value analysis done
    
    * removes redundant lower border value analyses
    
    * chronological order Upper border value analysis for generation Alpha
    
    * comment explaining func celebrityListContainsExpectedContent
    
    * function performHappyPathBirthYearTest(
      birthYear: string | number,
      expectedGeneration: RegExp
    )
    
    * using happy path func
    
    * happy path func at top
    
    * Error message if non-numerical birth year input
    
    * clearer structure
    
    * birth year param only number
    
    * more helper functions
    
    * todo & comment
    
    * happyPathBirthYearTest in commands
    
    * separates invalidBirthYearInputTest
    
    * separates try again
    
    * functions first, then add
    
    * js docs comments
    
    * range objects
    
    * asserts gen min year max year present
    
    * e2ehelpers.ts
    
    * helper functions not added to cy, used directly

--- client/cypress/e2e/generationFinder.cy.ts
+++ client/cypress/e2e/generationFinder.cy.ts
@@ -1,9 +1,129 @@
+import {
+  invalidBirthYearInputTest,
+  happyPathBirthYearTest,
+  clickTryAgainButton,
+} from '../support/e2eHelpers';
+
+const greatestGenerationRange = {
+  minYear: 1901,
+  maxYear: 1927,
+};
+const silentGenerationRange = {
+  minYear: 1928,
+  maxYear: 1945,
+};
+const boomerRange = {
+  minYear: 1946,
+  maxYear: 1964,
+};
+const genXRange = {
+  minYear: 1965,
+  maxYear: 1980,
+};
+const millenialRange = {
+  minYear: 1981,
+  maxYear: 1996,
+};
+const genZRange = {
+  minYear: 1997,
+  maxYear: 2012,
+};
+const generationAlphaRange = {
+  minYear: 2013,
+  maxYear: 2024,
+};
+
 describe('Tests of the generation finder', () => {
   beforeEach(() => {
     cy.visit('/');
   });
 
-  it('Expected title exists', () => {
-    cy.get('#generation-finder-top-heading').contains(/generation finder/i)
+  it('Error message if empty birth year', () => {
+    invalidBirthYearInputTest();
+  });
+
+  it('Error message if non-numerical birth year input', () => {
+    invalidBirthYearInputTest('non-numerical text');
+  });
+
+  // The Greatest Generation: 1901 - 1927
+
+  it('Lower border value analysis for greatest generation, 1900 & 1901', () => {
+    invalidBirthYearInputTest(1900);
+    happyPathBirthYearTest(
+      1901,
+      /greatest generation/i,
+      greatestGenerationRange
+    );
+  });
+
+  it('Upper border value analysis for greatest generation, 1927 & 1928', () => {
+    happyPathBirthYearTest(
+      1927,
+      /greatest generation/i,
+      greatestGenerationRange
+    );
+    clickTryAgainButton();
+    happyPathBirthYearTest(
+      1928,
+      /silent generation/i,
+      silentGenerationRange
+    );
+  });
+
+  // Lower border value analysis is only necessary for the earliest generation
+  // because the lower border value analysis of each generation is identical to
+  // the upper border value analysis of its previous generation
+
+  // The Silent Generation: 1928 - 1945
+
+  it('Upper border value analysis for Silent Generation, 1945 & 1946', () => {
+    happyPathBirthYearTest(
+      1945,
+      /silent generation/i,
+      silentGenerationRange
+    );
+    clickTryAgainButton();
+    happyPathBirthYearTest(1946, /boomer/i, boomerRange);
+  });
+
+  // Baby Boomers: 1946 - 1964
+
+  it('Upper border value analysis for Baby Boomers, 1964 & 1965', () => {
+    happyPathBirthYearTest(1964, /boomer/i, boomerRange);
+    clickTryAgainButton();
+    happyPathBirthYearTest(1965, /generation x/i, genXRange);
+  });
+
+  // Generation X: 1965 - 1980
+
+  it('Upper border value analysis for Generation X, 1980 & 1981', () => {
+    happyPathBirthYearTest(1980, /generation x/i, genXRange);
+    clickTryAgainButton();
+    happyPathBirthYearTest(1981, /millenial/i, millenialRange);
+  });
+
+  // Millenials: 1981 - 1996
+
+  it('Upper border value analysis for Millenials, 1996 & 1997', () => {
+    happyPathBirthYearTest(1996, /millenial/i, millenialRange);
+    clickTryAgainButton();
+    happyPathBirthYearTest(1997, /z/i, genZRange);
+  });
+
+  // Generation Z: 1997 - 2012
+
+  it('Upper border value analysis for Generation Z, 2012 & 2013', () => {
+    happyPathBirthYearTest(2012, /z/i, genZRange);
+    clickTryAgainButton();
+    happyPathBirthYearTest(2013, /generation alpha/i, generationAlphaRange);
+  });
+
+  // Generation Alpha: 2013 - 2024
+
+  it('Upper border value analysis for generation Alpha, 2024 & 2025', () => {
+    happyPathBirthYearTest(2024, /generation alpha/i, generationAlphaRange);
+    clickTryAgainButton();
+    invalidBirthYearInputTest(2025);
   });
 });
