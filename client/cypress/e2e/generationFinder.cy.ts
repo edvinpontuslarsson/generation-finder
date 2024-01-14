d4143b99672b9939ba7ca9617bf445a43b997f2c
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2024-01-14 18:06:21 +0100

    Remove duplicate tests

--- client/cypress/e2e/generationFinder.cy.ts
+++ client/cypress/e2e/generationFinder.cy.ts
@@ -46,14 +46,6 @@ describe('Tests of the generation finder', () => {
     invalidBirthYearInputTest('non-numerical text');
   });
 
-  it('Error message if non-numerical birth year input', () => {
-    cy.get('#birth-year-error-label').should('not.exist');
-
-    cy.get('#birth-year-input').clear().type('non-numerical text');
-    cy.get('#generation-finder-find-out-button').click();
-    cy.get('#birth-year-error-label').should('exist');
-  });
-
   // The Greatest Generation: 1901 - 1927
 
   it('Lower border value analysis for greatest generation, 1900 & 1901', () => {
@@ -134,50 +126,4 @@ describe('Tests of the generation finder', () => {
     clickTryAgainButton();
     invalidBirthYearInputTest(2025);
   });
-
-  it('Lower border analysis for greatest generation, 1900 & 1901', () => {
-    cy.get('#birth-year-input').clear().type('1900');
-    cy.get('#generation-finder-find-out-button').click();
-    cy.get('#birth-year-error-label').should('exist');
-
-    cy.get('#birth-year-input').clear().type('1901');
-    cy.get('#generation-finder-find-out-button').click();
-    cy.get('#birth-year-error-label').should('not.exist');
-
-    cy.get('#found-generation-heading').contains(/greatest generation/i);
-
-    cy.get('#celebrity-list')
-      .children()
-      .should('have.length.at.least', 1)
-
-      // each child element should contain expected content
-      .each((element) => {
-        cy.wrap(element).find('img').should('exist');
-
-        cy.wrap(element).find('a').should('not.be.empty');
-
-        cy.wrap(element)
-          .find('div[id^="celebrity-birth-year-"]')
-          .contains(/born in/i)
-          .should('exist');
-      });
-  });
-
-  it('Upper border analysis for greatest generation, 1927 & 1928', () => {
-    cy.get('#birth-year-input').clear().type('1927');
-    cy.get('#generation-finder-find-out-button').click();
-
-    cy.get('#found-generation-heading').contains(/greatest generation/i);
-    cy.celebrityListContainsExpectedContent();
-
-    cy.get('#found-generation-try-again-button').click();
-
-    cy.get('#birth-year-input').clear().type('1928');
-    cy.get('#generation-finder-find-out-button').click();
-
-    cy.get('#found-generation-heading').contains(/silent generation/i);
-    cy.celebrityListContainsExpectedContent();
-  });
-
-  // TODO do for generation alpha now also, special also
 });
